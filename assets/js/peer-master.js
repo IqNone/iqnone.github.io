4f8a6ecca5f0bf6bd10171a7c642fa9d02ba1e01
Adrian Iriciuc <adrian.iriciuc@aurea.com>
2024-09-10 19:23:54 +0300

    refactoring

--- assets/js/peer-master.js
+++ assets/js/peer-master.js
@@ -1,157 +1,156 @@
 function PeerMaster(name) {
-	const eventEmitter = new EventEmitter();
+    const eventEmitter = new EventEmitter();
 
-	this.on = (event, callback) =>  eventEmitter.on(event, callback);
+    this.on = (event, callback) => eventEmitter.on(event, callback);
 
-	const self = this;
-	const peer = new Peer(null, {
-		debug: 2
+    const peer = new Peer(null, {
+        debug: 2
     });
-	const clients = [];
+    const clients = [];
 
-	const session = {
-		state: 'idle',
-		description: '',
-		results: [],
-		players: []
-	}
+    const session = {
+        state: 'idle',
+        description: '',
+        results: [],
+        players: []
+    }
 
-	peer.on('open',  (id) => {
-		peer.id = id;
-		peer.name = name;
+    peer.on('open', (id) => {
+        peer.id = id;
+        peer.name = name;
 
-		session.players.push({name: peer.name, id: peer.id, master: true});
+        session.players.push({name: peer.name, id: peer.id, master: true});
 
-		eventEmitter.emit('start');
-	});
+        eventEmitter.emit('start');
+    });
 
-	peer.on('connection', function (connection) {
-		const client = {
-			connection,
-			id: connection.peer,
-			initialized: false
-		};
+    peer.on('connection', function (connection) {
+        const client = {
+            connection,
+            id: connection.peer,
+            initialized: false
+        };
 
-		clients.push(client);
+        clients.push(client);
 
         connection.on('data', (data) => {
-        	switch (data.op) {
-        		case 'name': 
-        			setClientName(client, data.name);
-        			break;
-        		case 'cast-vote':
-        			setClientVote(client, data.vote);
-        			break;
-        	}
+            switch (data.op) {
+                case 'name':
+                    setClientName(client, data.name);
+                    break;
+                case 'cast-vote':
+                    setClientVote(client, data.vote);
+                    break;
+            }
         });
 
-        connection.on('close',() => {
-        	const ic = clients.indexOf(client);
-        	if (ic >= 0) {
-        		clients.splice(ic, 1);
-        	}
-        	const ip = session.players.findIndex((p) => p.id === client.id);
+        connection.on('close', () => {
+            const ic = clients.indexOf(client);
+            if (ic >= 0) {
+                clients.splice(ic, 1);
+            }
+            const ip = session.players.findIndex((p) => p.id === client.id);
             if (ip >= 0) {
                 session.players.splice(ip, 1);
             }
-        	eventEmitter.emit('player-left', {id : client.id});
-        	sendAll({
-				player: {id : client.id},
-				op: 'player-left'
-			});
+            eventEmitter.emit('player-left', {id: client.id});
+            sendAll({
+                player: {id: client.id},
+                op: 'player-left'
+            });
         });
-	});
+    });
 
-	function setClientName(client, name) {
-		client.name = name;
-		const op = client.initialized ? 'player-name-changed' : 'player-joined';
-		const player = {
-			name,
-			id: client.id
-		};
+    function setClientName(client, name) {
+        client.name = name;
+        const op = client.initialized ? 'player-name-changed' : 'player-joined';
+        const player = {
+            name,
+            id: client.id
+        };
 
-		if (!client.initialized) {
-			client.connection.send({
-				op: 'session',
-				session: session
-			});
+        if (!client.initialized) {
+            client.connection.send({
+                op: 'session',
+                session: session
+            });
 
-			session.players.push(player);
-		}  else {
-			session.players.find((p) => player.id === p.id).name = name;
-		}
+            session.players.push(player);
+        } else {
+            session.players.find((p) => player.id === p.id).name = name;
+        }
 
-		client.initialized = true;
+        client.initialized = true;
 
-		eventEmitter.emit(op, player);
-		sendAll({op, player});
-	}
+        eventEmitter.emit(op, player);
+        sendAll({op, player});
+    }
 
-	function  setClientVote(client, vote) {
-		const player = session.players.find((p) => client.id === p.id);
-		player.vote = vote;
-		eventEmitter.emit('cast-vote', { vote, id : client.id });
-		sendAll({
-			op: 'cast-vote',
-			player: {
-				vote,
-				id: client.id,
-			}
-		});
-	}
+    function setClientVote(client, vote) {
+        const player = session.players.find((p) => client.id === p.id);
+        player.vote = vote;
+        eventEmitter.emit('cast-vote', {vote, id: client.id});
+        sendAll({
+            op: 'cast-vote',
+            player: {
+                vote,
+                id: client.id,
+            }
+        });
+    }
 
-	function sendAll(data) {
-		clients
-			.filter((c) => c.initialized)
-			.forEach((c) => c.connection.send(data));
-	}
+    function sendAll(data) {
+        clients
+            .filter((c) => c.initialized)
+            .forEach((c) => c.connection.send(data));
+    }
 
-	this.getId = () => peer.id;
-	this.setName = (name)  => {
-		peer.name = name;
-		session.players[0].name = name;
-		
-		event = {
-			op: 'player-name-changed',
-			player:{
-				name,
-				id: peer.id
-			}
-		};
+    this.getId = () => peer.id;
+    this.setName = (name) => {
+        peer.name = name;
+        session.players[0].name = name;
 
-		sendAll(event);
-		eventEmitter.emit(event.op, event.player)
-	}
+        event = {
+            op: 'player-name-changed',
+            player: {
+                name,
+                id: peer.id
+            }
+        };
 
-	this.startVoting = (description) => {
-		session.state = 'voting';
-		session.description = description;
-		session.players.forEach((p) => p.vote = null);
-		sendAll({
-			description,
-			op: 'start-voting'
-		});
-	}
+        sendAll(event);
+        eventEmitter.emit(event.op, event.player)
+    }
 
-	this.castVote = (v) => {
-		session.players[0].vote = v;
-		sendAll({
-			op: 'cast-vote',
-			player: {
-				id: peer.id,
-				vote: v
-			}
-		});
-	}
+    this.startVoting = (description) => {
+        session.state = 'voting';
+        session.description = description;
+        session.players.forEach((p) => p.vote = null);
+        sendAll({
+            description,
+            op: 'start-voting'
+        });
+    }
 
-	this.showResults = () => {
-		session.state = 'results';
-		sendAll({op: 'show-results'});
-	}
+    this.castVote = (v) => {
+        session.players[0].vote = v;
+        sendAll({
+            op: 'cast-vote',
+            player: {
+                id: peer.id,
+                vote: v
+            }
+        });
+    }
 
-	this.reset = () => {
-		session.state = 'idle';
-		session.description  = '';
-		sendAll({op: 'reset'});
-	}
+    this.showResults = () => {
+        session.state = 'results';
+        sendAll({op: 'show-results'});
+    }
+
+    this.reset = () => {
+        session.state = 'idle';
+        session.description = '';
+        sendAll({op: 'reset'});
+    }
 }
